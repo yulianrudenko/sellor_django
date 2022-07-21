d67dbfc0ab8f11ce17e87b224a3a9938a7171808
Yulian <yulian.rudenko.ukraine@gmail.com>
2022-07-21 23:19:28 +0200

    .gitignore updated

--- src/sellor/settings.py
+++ src/sellor/settings.py
@@ -16,7 +16,10 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'127.0.0.1',
+	'localhost',
+]
 
 
 # Application definition
